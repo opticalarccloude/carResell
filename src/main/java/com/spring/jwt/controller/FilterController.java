4e84d7229140db6786fee6f98faec6689dcbda9e
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2024-03-05 14:43:08 +0530

    Added : 05/03

---
+++ src/main/java/com/spring/jwt/controller/FilterController.java
@@ -0,0 +1,157 @@
+package com.spring.jwt.controller;
+
+import com.spring.jwt.Interfaces.FilterService;
+import com.spring.jwt.Interfaces.ICarRegister;
+import com.spring.jwt.Interfaces.PendingBookingService;
+import com.spring.jwt.Interfaces.UserService;
+import com.spring.jwt.dto.*;
+import com.spring.jwt.exception.CarNotFoundException;
+import com.spring.jwt.exception.PageNotFoundException;
+import com.spring.jwt.exception.UserNotFoundExceptions;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cars")
+public class FilterController {
+    @Autowired
+    private final FilterService filterService;
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ICarRegister iCarRegister;
+
+    private PendingBookingService pendingBookingService;
+
+
+    @GetMapping("/mainFilter/{pageNo}")
+    public ResponseEntity<ResponseAllCarDto> searchByFilter(
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) String area,
+            @RequestParam(required = false) String year,
+            @RequestParam(required = false) String brand,
+            @RequestParam(required = false) String model,
+            @RequestParam(required = false) String transmission,
+            @RequestParam(required = false) String fuelType,
+            @PathVariable int pageNo) {
+
+
+        Integer convertedYear = year != null && !year.isEmpty() ? Integer.valueOf(year) : null;
+
+
+
+        FilterDto filterDto = new FilterDto(minPrice, maxPrice, area, brand, model, transmission, fuelType, convertedYear);
+
+
+        try {
+            List<CarDto> listOfCar = filterService.searchByFilter(filterDto, pageNo);
+            ResponseAllCarDto responseAllCarDto = new ResponseAllCarDto("success");
+            responseAllCarDto.setList(listOfCar);
+            return ResponseEntity.status(HttpStatus.OK).body(responseAllCarDto);
+        } catch (PageNotFoundException pageNotFoundException) {
+            ResponseAllCarDto responseAllCarDto = new ResponseAllCarDto("unsuccess");
+            responseAllCarDto.setException("page not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseAllCarDto);
+        }
+    }
+
+    /**
+     * Retrieves a single car by its ID.
+     *
+     * @param carId The ID of the car to retrieve.
+     * @return ResponseEntity containing the response for the request.
+     *         If the car is found, the response will have a status of OK (200)
+     *         and the car details will be included in the body.
+     *         If the car is not found, the response will have a status of NOT_FOUND (404)
+     *         and an error message will be included in the body.
+     */
+    @GetMapping("/getCar")
+    public ResponseEntity<ResponseSingleCarDto> findByArea(@RequestParam int carId) {
+        try {
+            ResponseSingleCarDto responseSingleCarDto = new ResponseSingleCarDto("success");
+
+            CarDto car = iCarRegister.findById(carId);
+
+            responseSingleCarDto.setObject(car);
+            return ResponseEntity.status(HttpStatus.OK).body(responseSingleCarDto);
+        }catch (CarNotFoundException carNotFoundException){
+            ResponseSingleCarDto responseSingleCarDto = new ResponseSingleCarDto("unsuccess");
+            responseSingleCarDto.setException("car not found by car id");
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseSingleCarDto);
+        }
+
+//        return ResponseEntity.ok(cars.get());*
+    }
+    @GetMapping("/getAllCars")
+    public ResponseEntity<ResponseAllCarDto> getAllCars(@RequestParam int pageNo){
+        try
+        {
+            List<CarDto> listOfCar= filterService.getAllCarsWithPages(pageNo);
+
+            ResponseAllCarDto responseAllCarDto = new ResponseAllCarDto("success");
+            responseAllCarDto.setList(listOfCar);
+            return ResponseEntity.status(HttpStatus.OK).body(responseAllCarDto);
+        }
+        catch (CarNotFoundException carNotFoundException){
+//            List<CarDto> emptyList;
+            ResponseAllCarDto responseAllCarDto = new ResponseAllCarDto("unsuccess");
+            responseAllCarDto.setException(String.valueOf(carNotFoundException));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseAllCarDto);
+        }
+        catch (PageNotFoundException pageNotFoundException){
+            ResponseAllCarDto responseAllCarDto = new ResponseAllCarDto("unsuccess");
+            responseAllCarDto.setException(String.valueOf(pageNotFoundException));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseAllCarDto);
+        }
+    }
+    @PostMapping("/forgot-password")
+    public ResponseEntity<ResponseDto> forgotPass(HttpServletRequest request) throws UserNotFoundExceptions {
+        try {
+
+            String email = request.getParameter("email");
+
+            String token = RandomStringUtils.randomAlphabetic(40);
+
+            LocalDateTime expirationTime = LocalDateTime.now().plusMinutes(1);
+
+
+            userService.updateResetPassword(token, email);
+            String resetPasswordLink = "http://" + request.getServerName() + "/reset-password?token=" + token;
+
+            ResponseDto response = userService.forgotPass(email, resetPasswordLink, request.getServerName());
+
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("Successful", response.getMessage()));
+        } catch (UserNotFoundExceptions e) {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDto("Unsuccessful", "Invalid email. Please register."));
+        }
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<ResponseDto> resetPassword(@RequestBody ResetPassword resetPassword) throws UserNotFoundExceptions {
+
+        try {
+            String token = resetPassword.getToken();
+            String newPassword = resetPassword.getPassword();
+            ResponseDto response = userService.updatePassword(token, newPassword);
+
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("Successful", response.getMessage()));
+
+        } catch (UserNotFoundExceptions e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDto("Unsuccessful", "Something went wrong"));
+        }
+    }
+
+}
---
+++ src/main/java/com/spring/jwt/repository/CarRepo.java
@@ -0,0 +1,37 @@
+package com.spring.jwt.repository;
+
+
+import com.spring.jwt.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CarRepo extends JpaRepository<Car, Integer>, JpaSpecificationExecutor<Car> {
+
+    @Query(value = "SELECT * FROM car WHERE area = ?1", nativeQuery = true)
+    Optional<List<Car>> FindByArea(@Param("area") String area);
+
+    @Query("SELECT c FROM Car c WHERE c.price > :minPrice AND c.price < :maxPrice AND c.area = :area AND c.year = :year AND c.brand = :brand AND c.model = :model AND c.transmission = :transmission AND c.fuelType = :fuelType")
+    Optional<List<Car>> findCarsByParameters(@Param("minPrice") int minPrice, @Param("maxPrice") int maxPrice, @Param("area") String area, @Param("year") int year, @Param("brand") String brand, @Param("model") String model, @Param("transmission") String transmission, @Param("fuelType") String fuelType);
+
+//    @Query("SELECT c FROM Car c WHERE c.dealerId = :dealerId AND c.carStatus = :status")
+//    List<Car> findCarsByDealerIdAndStatus(@Param("dealerId") int dealerId, @Param("status") String status);
+//    @Query("SELECT c FROM Car c WHERE dealerId = :dealerId and carStatus = 'pending'")
+//    @Query("SELECT c FROM Car c WHERE c.dealerId= :dealerId AND c.carStatus= :carStatus")
+
+    @Query(value = "SELECT * FROM car c WHERE c.dealer_id = :dealerId AND c.car_status = :carStatus", nativeQuery = true)
+    List<Car> findByDealerIdAndCarStatus(@Param("dealerId") Integer dealerId, @Param("carStatus") String carStatus);
+
+    @Query("SELECT c FROM Car c WHERE carStatus='active' OR carStatus='pending'")
+    public List<Car> getPendingAndActivateCar();
+
+    public Optional<List<Car>> getByDealerId(Integer dealerId);
+}
+
+
