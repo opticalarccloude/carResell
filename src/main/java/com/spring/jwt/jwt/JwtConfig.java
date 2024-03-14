c6a7a06f7829c23c49f38c8ba402bdbe5989ec07
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2024-03-14 15:26:39 +0530

    ok

--- src/main/java/com/spring/jwt/jwt/JwtConfig.java
+++ src/main/java/com/spring/jwt/jwt/JwtConfig.java
@@ -20,4 +20,12 @@ public class JwtConfig {
 
     @Value("${jwt.secret:3979244226452948404D6251655468576D5A7134743777217A25432A462D4A61}")
     private String secret;
+
+    public JwtConfig() {
+        this.url = "/jwt/login";
+        this.header = "Authorization";
+        this.prefix = "Bearer";
+        this.expiration = 10*180;
+        this.secret = "3979244226452948404D6251655468576D5A7134743777217A25432A462D4A61";
+    }
 }
